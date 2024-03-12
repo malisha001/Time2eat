ff118a2904a96f9068befdc2331be48b4f73d056
Bhanuka-Fernando <bhanukafernando2@gmail.com>
2024-03-12 06:58:21 +0530

    Started backend

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,5 @@
-const express = require('express')
+const express = require('express');
 
-//express app
+//invoke express app
 const app = express()
 
-//middlewre to parse incomming JSON data
-app.use(express.json())
-
-app.use((req,res,next)=>{
-    console.log(req.path,res.methode);
-    next()
-})
-
-//routers
-
-//connect db
-mongoose.connect(process.env.MONG_URI)
-    .then(()=> {
-        //listen request
-        app.listen(process.env.PORT, ()=>{
-        console.log("listening on port",process.env.PORT);
-        console.log("db connected successfully");
-        })
-    })
-    .catch((error) =>{
-        console.log(error);
-})
