6f98d8913e905febc937c7b01b62c5e79e70e65d
Malisha Sandeep <121741613+malisha001@users.noreply.github.com>
2024-03-17 22:31:22 +0530

    basic crud

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config()
+const employeesal = require('./routers/employeeSalary')
 
 // express app
 const app = express();
@@ -15,7 +16,7 @@ app.use((req, res, next) => {
 });
 
 // routers
-
+app.use('/api/employeesal',employeesal)
 // connect to db
 mongoose.connect(process.env.MONG_URI)
     .then(() => {
