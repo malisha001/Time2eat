f5f4ed07822acffb80be855c7176b94f51f06625
Bhanuka-Fernando <bhanukafernando2@gmail.com>
2024-03-18 15:55:57 +0530

    img

--- backend/model/inventoryModel.js
+++ backend/model/inventoryModel.js
@@ -24,28 +24,16 @@ const inventorySchema = new Schema({
         type: String,
         required: true
     },
-<<<<<<< HEAD
+
     
     itemImage: {
         data: Buffer, 
         contentType: String 
-=======
-    itemImage: {
-        data: {
-            type: Buffer
-        },
-        contentType: {
-            type: String
-        }
->>>>>>> c5da74e2c979fbf0bb7d96d7a0ac0e2cad7c7b50
     }
-}, { timestamps: true });
+}, { Timestamp: true });
 
-<<<<<<< HEAD
 
-}, {Timestamp: true})
+
 
 module.exports = mongoose.model('Inventory', inventorySchema )
-=======
-module.exports = mongoose.model('Inventory', inventorySchema);
->>>>>>> c5da74e2c979fbf0bb7d96d7a0ac0e2cad7c7b50
+
