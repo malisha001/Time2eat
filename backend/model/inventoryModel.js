c5da74e2c979fbf0bb7d96d7a0ac0e2cad7c7b50
chala16 <chalanakaveeshami@gmail.com>
2024-03-18 14:46:38 +0530

    do little change in the model

--- backend/model/inventoryModel.js
+++ backend/model/inventoryModel.js
@@ -1,41 +1,37 @@
-const { Timestamp } = require('mongodb')
-const mongoose = require('mongoose')
+const { Timestamp } = require('mongodb');
+const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const inventorySchema = new Schema({
-
     itemId: {
         type: Number,
         required: true
     },
-
     itemName: {
         type: String,
         required: true
     },
-
     itemQuantity: {
         type: Number,
         required: true
     },
-
     itemPrice: {
         type: Number,
         required: true
     },
-
     itemCategory: {
         type: String,
         required: true
     },
-
     itemImage: {
-        data: Buffer,
-        contentType: String,
-        required: false
+        data: {
+            type: Buffer
+        },
+        contentType: {
+            type: String
+        }
     }
+}, { timestamps: true });
 
-}, {timestamps: true})
-
-module.exports = mongoose.model('Inventory', inventorySchema )
+module.exports = mongoose.model('Inventory', inventorySchema);
